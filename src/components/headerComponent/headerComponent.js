a8b81f9baf420546ecf73b9a1008f370dea1af8c
Samuel Muniz <137716288+DevSamuelMuniz@users.noreply.github.com>
2024-05-03 21:52:09 -0300

    asd

--- src/components/headerComponent/headerComponent.js
+++ src/components/headerComponent/headerComponent.js
@@ -1,27 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./headerComponent.css";
 
-function headerComponent() {
+function HeaderComponent() {
+  const [selectedButton, setSelectedButton] = useState(null);
+
+  const handleButtonClick = (index) => {
+    setSelectedButton(index);
+  };
+
   return (
     <main className="content-header">
       <ul className="ul-header">
-        <button className="button-header">
+        <button
+          className={`button-header ${selectedButton === 0 ? "selected" : ""}`}
+          onClick={() => handleButtonClick(0)}
+        >
           <li className="li-header">Início</li>
         </button>
 
-        <button className="button-header">
+        <button
+          className={`button-header ${selectedButton === 1 ? "selected" : ""}`}
+          onClick={() => handleButtonClick(1)}
+        >
           <li className="li-header">Portfólio</li>
         </button>
 
-        <button className="button-header">
+        <button
+          className={`button-header ${selectedButton === 2 ? "selected" : ""}`}
+          onClick={() => handleButtonClick(2)}
+        >
           <li className="li-header">Sobre mim</li>
         </button>
 
-        <button className="button-header">
+        <button
+          className={`button-header ${selectedButton === 3 ? "selected" : ""}`}
+          onClick={() => handleButtonClick(3)}
+        >
           <li className="li-header">Resumo</li>
         </button>
 
-        <button className="button-header">
+        <button
+          className={`button-header ${selectedButton === 4 ? "selected" : ""}`}
+          onClick={() => handleButtonClick(4)}
+        >
           <li className="li-header">Contatos</li>
         </button>
       </ul>
@@ -29,4 +50,4 @@ function headerComponent() {
   );
 }
 
-export default headerComponent;
+export default HeaderComponent;
