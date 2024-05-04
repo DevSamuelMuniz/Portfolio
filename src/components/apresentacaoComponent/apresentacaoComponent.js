2f9268ceff5ba6e35a0e357001ca14d23c8de9ec
Samuel Muniz <137716288+DevSamuelMuniz@users.noreply.github.com>
2024-05-04 18:36:00 -0300

    add apresentação component

--- src/components/apresentacaoComponent/apresentacaoComponent.js
+++ src/components/apresentacaoComponent/apresentacaoComponent.js
@@ -1,23 +1,29 @@
 import React from "react";
+import "./apresentacaoComponent.css"
 
-import { FaStore } from "react-icons/fa";
+import { FaBriefcase, FaDownload } from "react-icons/fa";
 
 function apresentacaoComponent() {
   return (
     <main>
       <div>
-        <h1>Vamos nos conhecer</h1>
-        <h1>Eu sou Samuel Muniz desenvolvedor Frontend e Backend.</h1>
+        <h1 className="conhecer-ctn">Vamos nos conhecer!</h1>
+        <h1 className="title-apres">Eu sou Samuel Muniz desenvolvedor Frontend e Backend.</h1>
       </div>
-      <div>
-        <a>
-          Meu trabalhos{" "}
-          <span>
-            <FaStore />
+
+      <div className="ctn-btn">
+        <a href="" className="btn-apres">
+          Meu trabalhos
+          <span className="ico">
+            <FaBriefcase />
           </span>
         </a>
-        <a>
-          Baixe meu currículo <span></span>
+
+        <a href="" className="btn-apres">
+          Baixe meu currículo
+          <span className="ico">
+            <FaDownload />
+          </span>
         </a>
       </div>
     </main>
